04f2cd315c2153fb34d5eb1a95c61d419d9e486a
CrisTa <canh.tc98@gmail.com>
2022-06-01 11:28:06 +0700

    feat: update readme

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Store API')
+    .setTitle('Fake Clothing APIs')
     .setDescription('The Fake Fashion Store API description')
     .setVersion('1.0')
-    .addTag('store')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
