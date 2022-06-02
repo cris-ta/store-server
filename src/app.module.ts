9e4d3fcb754902b8948a049c5880cf5b9abc1865
CrisTa <canh.tc98@gmail.com>
2022-06-02 20:32:18 +0700

    fet: config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,29 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './modules/products/products.module';
 import { UsersModule } from './modules/users/users.module';
 import { CartsModule } from './modules/carts/carts.module';
-import { ConfigModule } from './modules/config/config.module';
 import { AuthGuard } from './guards/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.register({
-      folder: './config',
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        ssl: true,
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     ProductsModule,
     UsersModule,
